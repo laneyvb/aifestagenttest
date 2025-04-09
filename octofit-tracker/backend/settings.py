58b44d2ad467b3d62bd873924975fb50165ab94c
Elaine van Bergen <elvanber@microsoft.com>
2025-04-09 07:04:55 +0000

    Set up backend requirements and initial settings for OctoFit Tracker

---
+++ octofit-tracker/backend/settings.py
@@ -0,0 +1,7 @@
+# filepath: /workspaces/aifestagenttest/octofit-tracker/backend/<your_project_name>/settings.py
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'octofit_db',  # Name of your MongoDB database
+    }
+}
